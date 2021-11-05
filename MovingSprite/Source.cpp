5cf18f40819eaf74b77e36531aa7be688b042ea0
Nastya Ber <anastasiaberz212@gmail.com>
2021-11-05 07:33:59 +0300

    add animation mode

--- MovingSprite/Source.cpp
+++ MovingSprite/Source.cpp
@@ -1,15 +1,22 @@
 #include <Windows.h>
+#include <windowsx.h>
 
 RECT actorRC{ 200, 200, 250, 250 };
 
 int winWidth = 600;
 int winHeight = 600;
 
-RECT clienRC;
+RECT sceneRC;
 RECT windowRC{ 0, 0, winWidth, winHeight };
 HDC hdc;
 
-float moveByButton = 5;
+int moveByButton = 3;
+
+const int ANIM_BUTTON = 1234;
+
+bool anim = false;
+
+POINT speed{ 3, 2};
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -20,24 +27,24 @@ bool isShift()
 	return FALSE;
 }
 
-void WinShow(HDC hdc)
+void Show(HDC hdc)
 {
 	HDC memDC = CreateCompatibleDC(hdc);
-	HBITMAP memBM = CreateCompatibleBitmap(hdc, windowRC.right - windowRC.left, windowRC.bottom - windowRC.top);
+	HBITMAP memBM = CreateCompatibleBitmap(hdc, sceneRC.right - sceneRC.left, sceneRC.bottom - sceneRC.top);
 	SelectObject(memDC, memBM);
 
 	HBRUSH hbrBkGnd = CreateSolidBrush(GetSysColor(COLOR_WINDOW));
-	FillRect(memDC, &windowRC, hbrBkGnd);
+	FillRect(memDC, &sceneRC, hbrBkGnd);
 
 	HBRUSH hbrActor = CreateSolidBrush(RGB(0, 99, 71));
 	FillRect(memDC, &actorRC, hbrActor);
 
-	BitBlt(hdc, 0, 0, windowRC.right - windowRC.left, windowRC.bottom - windowRC.top, memDC, 0, 0, SRCCOPY);
+	BitBlt(hdc, 0, 60, sceneRC.right - sceneRC.left, sceneRC.bottom - sceneRC.top, memDC, 0, 0, SRCCOPY);
 }
 
 void moveHor(int move)
 {
-	if ((move > 0 && actorRC.right < clienRC.right) || (move < 0 && actorRC.left > clienRC.left))
+	if ((move > 0 && actorRC.right < sceneRC.right) || (move < 0 && actorRC.left > sceneRC.left))
 	{
 		actorRC.left += move;
 		actorRC.right += move;
@@ -46,25 +53,56 @@ void moveHor(int move)
 
 void moveVert(int move)
 {
-	if ((move > 0 && actorRC.bottom < clienRC.bottom) || (move < 0 && actorRC.top > clienRC.top))
+	if ((move > 0 && actorRC.bottom < sceneRC.bottom) || (move < 0 && actorRC.top > sceneRC.top))
 	{
 		actorRC.top += move;
 		actorRC.bottom += move;
 	}
 }
 
+void animate()
+{
+	if (actorRC.bottom > sceneRC.bottom || actorRC.top < sceneRC.top)
+		speed.y *= -1;
+	if (actorRC.right > sceneRC.right || actorRC.left < sceneRC.left)
+		speed.x *= -1;
+
+	actorRC.left += speed.x;
+	actorRC.right += speed.x;
+	actorRC.top += speed.y;
+	actorRC.bottom += speed.y;
+
+	/*if (speed.x != 0 && speed.y != 0) {
+		speed.x = speed.x * 0.7;
+		speed.y = speed.y * 0.7;
+	}*/
+}
+
 void Update()
 {
-	if ((GetKeyState(VK_LEFT) & 0x8000) != 0)
-		moveHor(-moveByButton);
-	if ((GetKeyState(VK_RIGHT) & 0x8000) != 0)
-		moveHor(moveByButton);
-	if ((GetKeyState(VK_UP) & 0x8000) != 0)
-		moveVert(-moveByButton);
-	if ((GetKeyState(VK_DOWN) & 0x8000) != 0)
-		moveVert(moveByButton);
+	if (anim)
+	{
+		animate();
+	}
+	else
+	{
+		if ((GetKeyState(VK_LEFT) & 0x8000) != 0)
+			moveHor(-moveByButton);
+		if ((GetKeyState(VK_RIGHT) & 0x8000) != 0)
+			moveHor(moveByButton);
+		if ((GetKeyState(VK_UP) & 0x8000) != 0)
+			moveVert(-moveByButton);
+		if ((GetKeyState(VK_DOWN) & 0x8000) != 0)
+			moveVert(moveByButton);
+	}
 
-	WinShow(hdc);
+	Show(hdc);
+}
+
+void GetDrawableRect(HWND hwnd)
+{
+	GetClientRect(hwnd, &sceneRC);
+	sceneRC.bottom -= 60;
 }
 
 int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int nCmdShow)
@@ -93,12 +131,10 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int nCmdS
 		0,
 		wc.lpszClassName,
 		L"Mooving Sprite",
-		WS_DLGFRAME | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZE,
+		WS_OVERLAPPEDWINDOW,
 		(GetSystemMetrics(SM_CXSCREEN) - windowRC.right) / 2,
 		(GetSystemMetrics(SM_CYSCREEN) - windowRC.bottom) / 2,
-		winWidth,
-		winHeight,
-		nullptr, nullptr, nullptr, wc.hInstance);
+		winWidth, winHeight, nullptr, nullptr, nullptr, wc.hInstance);
 
 	if (hwnd == INVALID_HANDLE_VALUE)
 		return EXIT_FAILURE;
@@ -131,19 +167,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		case WM_CREATE:
 		{
-			GetClientRect(hWnd, &clienRC);
+			HWND animButton = CreateWindow(
+				L"BUTTON",
+				L"Show Animation",
+				WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+				10,	10,	150, 40,
+				hWnd,
+				reinterpret_cast<HMENU>(ANIM_BUTTON),
+				nullptr,
+				nullptr
+			);
+
+			GetDrawableRect(hWnd);
+		}
+		return 0;
+
+		case WM_COMMAND:
+		{
+			switch (LOWORD(wParam))
+			{
+			case ANIM_BUTTON:
+			{
+				anim = !anim;
+			}
 			break;
+			}
+		}
+		return 0;
+
+		case WM_SIZE: {
+			GetDrawableRect(hWnd);
 		}
 		return 0;
 
 		case WM_MOUSEWHEEL:
 		{
-			int move = GET_WHEEL_DELTA_WPARAM(wParam) / 20;
-			if (isShift()) {
-				moveHor(move);
-			}
-			else {
-				moveVert(move);
+			if (!anim)
+			{
+				int move = GET_WHEEL_DELTA_WPARAM(wParam) / 20;
+				if (isShift()) {
+					moveHor(move);
+				}
+				else {
+					moveVert(move);
+				}
 			}
 		}
 		return 0;
